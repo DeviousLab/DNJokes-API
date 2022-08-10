8deecb56af0f709ad6d86d07883e4a256f0c81fa
DeviousLab <deviouslab@gmail.com>
2022-08-11 00:28:59 +0400

    Implemented method to get joke by id
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from supabase import create_client, Client
 from dotenv import load_dotenv
+from pydantic import BaseModel, Json
 
 load_dotenv()
 app = FastAPI()
@@ -11,28 +12,28 @@ url = os.getenv('SUPABASE_SUPAFAST_URL')
 key = os.getenv('SUPABASE_SUPAFAST_KEY')
 supabase: Client = create_client(url, key)
 
-@app.get("/jokes", status_code=200)
-def jokes():
-    jokes = supabase.table('Jokes').select('*').execute()
+@app.get("/")
+def index():
+    return {
+        "message": "Please refer to the documentation for more or visit http://localhost:8000/docs"
+    }
+
+@app.get("/jokes", status_code=200) #done
+def all_jokes():
+    jokes = supabase.table('Jokes').select('prompt').execute()
     return jokes
 
-@app.get("/joke", status_code=200)
-def jokes():
-    jokes = supabase.table('Jokes').select('*').execute()
+@app.get("/joke/search", status_code=200) 
+def search_jokes(keyword: Optional[str] = None):
+        jokes = supabase.table('Jokes').select('*').eq('prompt', keyword).execute()
+        return jokes
+
+@app.get("/joke/{id}", status_code=200) #done
+def id_joke(id: int):
+    jokes = supabase.table('Jokes').select('*').eq('id', id).execute()
     return jokes
 
 @app.get("/joke/random", status_code=200)
-def jokes():
+def random_joke():
     jokes = supabase.table('Jokes').select('*').execute()
     return jokes
-
-@app.get("/joke/{id}", status_code=200)
-def jokes():
-    jokes = supabase.table('Jokes').select('*').execute()
-    return jokes
-
-@app.get("/joke/search", status_code=200)
-def search_jokes(keyword: Optional[str] = None, max_results: Optional[int] = 10) -> dict:
-    jokes = supabase.table('Jokes').select('*').execute()
-    return jokes 
-    
