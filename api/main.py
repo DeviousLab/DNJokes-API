a20eb7810d27d12abc7cfdf0d4cdd5186a6ea56d
DeviousLab <deviouslab@gmail.com>
2022-08-11 23:27:38 +0400

    Implemented method to search joke by prompt
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 from typing import Optional
 from fastapi import FastAPI
 from supabase import create_client, Client
@@ -19,17 +20,27 @@ def index():
     }
 
 @app.get("/jokes", status_code=200) #done
-def all_jokes(max_results: Optional[int] = 10):
+def all_jokes(max_results: Optional[int] = 47):
     jokes = supabase.table('Jokes').select('*').limit(max_results).execute()
 
     return jokes
 
 @app.get("/joke/search", status_code=200) 
 def search_jokes(keyword: Optional[str] = None):
+    if keyword is None:
+        return supabase.table('Jokes').select('*').execute()
+    else:
+        query_results = []
         jokes = supabase.table('Jokes').select('*').execute()
-        for index, row in jokes:
-            if keyword in row[0]:
-                return row
+
+        for index in jokes.data:
+                if re.search(keyword, index['prompt'], re.IGNORECASE):
+                        query_results.append(index)
+                # else:
+                #         return {
+                #             "message": "No matches found for '{query}'".format(query=keyword)
+                #         }
+        return query_results
 
 @app.get("/joke/random", status_code=200) #done
 def random_joke():
